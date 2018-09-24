07b5aa04fc88523c93762c9cf962fd1f68846af5
Grimmer <grimmer0125@gmail.com>
2018-09-24 11:40:43 +0800

    Clean up code and settings

--- src/services/response/index.js
+++ src/services/response/index.js
@@ -12,15 +12,3 @@ export const notFound = (res) => (entity) => {
   res.status(404).end()
   return null
 }
-
-export const authorOrAdmin = (res, user, userField) => (entity) => {
-  if (entity) {
-    const isAdmin = user.role === 'admin'
-    const isAuthor = entity[userField] && entity[userField].equals(user.id)
-    if (isAuthor || isAdmin) {
-      return entity
-    }
-    res.status(401).end()
-  }
-  return null
-}
--- test/setup.js
+++ test/setup.js
@@ -1,25 +1,7 @@
-import { EventEmitter } from 'events'
 import MongodbMemoryServer from 'mongodb-memory-server'
 import mongoose from '../src/services/mongoose'
 
-EventEmitter.defaultMaxListeners = Infinity
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
-
-global.Array = Array
-global.Date = Date
-global.Function = Function
-global.Math = Math
-global.Number = Number
-global.Object = Object
-global.RegExp = RegExp
-global.String = String
-global.Uint8Array = Uint8Array
-global.WeakMap = WeakMap
-global.Set = Set
-global.Error = Error
-global.TypeError = TypeError
-global.parseInt = parseInt
-global.parseFloat = parseFloat
+jest.setTimeout(10000)
 
 let mongoServer
 
