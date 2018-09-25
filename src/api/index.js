54f668a4c6c678a1e8c21ea701417e7f0de8c6ac
Grimmer <grimmer0125@gmail.com>
2018-09-25 23:55:59 +0800

    Modify API desc

--- src/api/index.js
+++ src/api/index.js
@@ -5,4 +5,9 @@ const router = new Router()
 
 router.use('/vdobject', vdobject)
 
+/**
+ * @apiDefine listParams
+ * @apiParam {Number} [timestamp] Unixtime second unit (e.g. 1537693722).
+ */
+
 export default router
